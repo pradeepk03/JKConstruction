42fc776025b81d0f514983c7db0f60ce8d38207a
Pradeep Kunder <kunderpradeep03@gmail.com>
2019-06-02 10:34:39 +0530

    server js

--- JkServer/server.js
+++ JkServer/server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express");//express module
 const mysql = require("mysql");
 const cors = require('cors')
 const path = require('path');
